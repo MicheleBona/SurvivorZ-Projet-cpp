8f911221b324f23568c062db6d44b044ba727de8
MicheleBona <45558452+MicheleBona@users.noreply.github.com>
2020-12-26 23:00:44 +0100

    Update communaute.hh

--- code/communaute.hh
+++ code/communaute.hh
@@ -2,16 +2,17 @@
 #include <string>
 #include <iostream>
 #include <random>
+#include <vector>
 #include "equipement.hh"
 #include "survivant.hh"
 
 class communaute
 {
 public:
-	communaute(std::string nom);
+	communaute(std::string Nom);
 	~communaute();
 	// Getters
-	std::string getNom() const { return this -> nom;}
+	std::string getNom() const { return this -> c_nom;}
 	int getJourSurvecus() const { return this -> jourSurvecus;}
 	int getNbNourriture() const { return this -> nbNourriture;}
 	int getNbEau() const { return this -> nbEau;}
@@ -19,18 +20,23 @@ public:
 	int getNbJardins() const { return this -> nbJardins;}
 	// Surcharge
 	// Methodes
-	void resumeInfos();
+	void ajouterMembre();
+	void resumeInfos() const;
 	void EnvoyerChercherDesVivres(survivant & s, equipement & e);
 	void FaireConstruire(survivant & s, equipement & e);
 	void Soigner(survivant & soigneur, survivant & cible);
-	void ActionsRestantes(); // return s travaille FALSE
+	void ActionsRestantes() const; // return s travaille FALSE
 	void FinirJour();
 private:
-	std::string nom;
+	std::string c_nom;
 	int jourSurvecus;
 	int nbNourriture;
 	int nbEau;
 	int nbMedicaments;
 	int nbJardins;
-	//survivant * membres;
+	std::vector<survivant> membres;
+	std::vector<survivant> equipements;
+	/*vector<survivant *> listeSurvivants;
+	listeSurvivants.push_back(new survivant(arguments)); 
+	SI PLUSIEURS TYPES DE SURVIVANTS PRATIQUE*/
 };
